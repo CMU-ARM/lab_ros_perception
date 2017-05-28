b4b0e0c657e217fa5c46a3259cad2d9271a560f6
Zhi <xiangzhi@users.noreply.github.com>
2017-05-27 21:09:54 -0400

    Added ability see multiple tags and check IDs

--- python_src/lab_ros_perception/AprilTagModule.py
+++ python_src/lab_ros_perception/AprilTagModule.py
@@ -43,8 +43,64 @@ class AprilTagModule(object):
 
 		"""
 		
-		if id_ in self.detected_tags:
+		if self.checkID(id_):
 			return self.detected_tags[id_]
 		else:
 			rospy.loginfo("ID never been seen")
 			return None
+		
+	def getPosesForIDs(self, ids):
+		"""
+		Returns the list of last stamped pose for the aprilTag for the given ids. Individual element will be None if 
+		not seen before
+		
+		Parameters
+		----------
+		ids : int[]
+			list of IDs to be search for
+
+		Returns:
+		geomtery/StampedPose[]
+			list of StampedPose for the given ID last seen by April Tag Node
+		"""
+		
+		pose_list = []
+		for id_ in ids:
+			pose_list.append(self.getPoseForID(id_))
+		return pose_list
+	
+	def checkID(self, id_):
+		"""
+		Check whether the module has seen the ID
+		
+		Parameters
+		----------
+		id_ : int
+			ID of the tag looking for
+
+		Returns:
+		Bool
+			True if ID has been seen, False otherwise
+		"""
+		return id_ in self.detected_tags
+	
+	def getTimeForID(self, ids):
+		"""
+		Returns the time stamp of the AprilTag was last seen, none otherwise.
+		
+		Parameters
+		----------
+		id_ : int
+			ID of the tag
+			
+		Returns:
+		Time
+			Two-integer timestamp of the last time the AprilTag was seen
+
+		"""
+		if self.checkID(id_):
+			return self.detected_tags[id_].header.stamp
+		else:
+			rospy.loginfo("ID never been seen")
+			return None
+		
